db35dd7254440e263e2dcd5a420077f820ad1ffd
Armun Alam <armunalam@gmail.com>
2021-01-23 00:44:23 +0600

    update semester

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Student_T)
+admin.site.register(Faculty_T)
+admin.site.register(School_T)
+admin.site.register(Department_T)
+admin.site.register(Program_T)
+admin.site.register(Course_T)
+admin.site.register(Evaluation_T)
+admin.site.register(Assessment_T)
+admin.site.register(CO_T)
+admin.site.register(PLO_T)
+admin.site.register(Enrollment_T)
+admin.site.register(Section_T)
